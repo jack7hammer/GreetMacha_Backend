4e88620e87b322bfc2bb1f74db808a9618805887
jack7hammer <jackhammer1433@gmail.com>
2020-12-14 00:28:00 +0530

    changes made : Image file uploaded will be in preview without storing in database. Modified in birthday_templte, userDetailsForm.html , views.py

--- templates/views.py
+++ templates/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import storedInformation
 import json
+from base64 import b64encode
 
 # Create your views here.
 def home(request):
@@ -11,6 +12,10 @@ def t1(request):
 	if request.method=='POST':
 		name=request.POST['bfname']
 		message=request.POST['message']
-		context={'name':name,'message':message}
-
+		inImg = request.FILES["inputImage"].read()
+		encoded = b64encode(inImg).decode('ascii')
+		mime = "image/jpg"
+		mime = mime + ";" if mime else ";"
+		input_image = "data:%sbase64,%s" % (mime, encoded)
+		context={'name':name,'message':message, 'input_image':input_image}
 	return render(request,"templates/birthday_template.html", context)
